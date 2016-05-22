5182df3185aac13942215fd0c96042a1b653eab0
ksgwr <ksgwarai@gmail.com>
2016-05-22 17:24:48 +0900

    fix redisEnable

--- test/dic_test.js
+++ test/dic_test.js
@@ -4,18 +4,18 @@ var Dic = require('../lib/dic');
 
 describe('Dic', function() {
     it('load', function(done) {
-	var dic = new Dic();
-	dic.load('public/dic/dic.tsv', 'utf-8', function(entries) {
+        var dic = new Dic();
+        dic.load('public/dic/dic.tsv', 'utf-8', function(entries) {
             assert.equal('fuga', entries['hoge']);
-	    done();
-	});
+            done();
+        });
     });
     it('put', function(done) {
-	var dic = new Dic();
-	dic.load('public/dic/dic.tsv', 'utf-8', function(entries) {
-	    dic.put('aaa', 'bbb', function() {
-	    	done();
-	    });
-	});
+        var dic = new Dic();
+        dic.load('public/dic/dic.tsv', 'utf-8', function(entries) {
+            dic.put('aaa', 'bbb', function() {
+                done();
+            });
+        });
     });
 });
