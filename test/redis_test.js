6fccdb949b019e7e50dd9afa5b32a0ed230a4225
ksgwr <ksgwarai@gmail.com>
2016-05-22 23:13:10 +0900

    fix test

--- test/redis_test.js
+++ test/redis_test.js
@@ -4,14 +4,18 @@ var dic = new Dic();
 var cloudUrl = process.env.REDISCLOUD_URL;
 dic.setRedisClient(cloudUrl);
 
+if (process.env.REDISCLOOUD_URL) {
+
+
 describe('redis', function() {
     it('enable redis', function() {
-        assert.equal(true, dic.enableRedis);
+            assert.equal(true, dic.enableRedis);
     });
     it('get test', function(done) {
         dic.load('public/dic/dic.tsv', 'utf-8', function(entries) {
-            console.log(entries);
             done();
         });
     });
 });
+
+}
